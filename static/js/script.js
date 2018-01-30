9a4b33535d2fe372c1353e512723bc2723006563
mhernan2 <mhernan2@unb.ca>
2018-01-30 06:46:38 -0400

    changed appearance, added html pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,7 @@
 $(function() {
-	
+	$('.sub-menu ul').hide();
+	$('.sub-menu a').click(function () {
+		$(this).parent(".sub-menu").children("ul").slideToggle("slow");
+		$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+	});
 });
