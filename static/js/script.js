339ceb52a03080767fdce12fdfaebd48779291d3
mhernan2@unb.ca <mhernan2@unb.ca>
2018-11-03 23:13:24 -0300

    deleting unnecessary files

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$('.sub-menu ul').hide();
-	$('.sub-menu a').click(function () {
-		$(this).parent(".sub-menu").children("ul").slideToggle("slow");
-		$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
-	});
-});
